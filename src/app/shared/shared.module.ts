0f1bf53043060b8f7e3f0fcbab4e915a340b762b
Junior <junior.silva_costa@live.com>
2022-05-17 22:08:19 -0300

    :sparkles: adding footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AllPostsComponent } from './components/all-posts/all-posts.component';
 import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LatestPostsComponent } from './components/latest-posts/latest-posts.component';
 import { PopularPostsComponent } from './components/popular-posts/popular-posts.component';
@@ -14,6 +15,7 @@ const COMPONENTS = [
   RecommendedPostsComponent,
   AllPostsComponent,
   PopularPostsComponent,
+  FooterComponent,
 ];
 
 @NgModule({
